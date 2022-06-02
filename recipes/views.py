7262fb355517b8137a66a0895c23c218773336b2
NathanTelles21 <nathan.rodrigues.124@ufrn.edu.br>
2022-06-02 19:05:35 -0300

    alterações

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(12)]
