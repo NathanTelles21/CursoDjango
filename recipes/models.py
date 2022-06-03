ceae0a0270042cebe451148876c618778aec29c0
NathanTelles21 <nathan.rodrigues.124@ufrn.edu.br>
2022-06-02 22:57:45 -0300

    atualizações

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,13 @@
 
-from distutils.command import upload
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -17,4 +24,10 @@ class Recipe(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
